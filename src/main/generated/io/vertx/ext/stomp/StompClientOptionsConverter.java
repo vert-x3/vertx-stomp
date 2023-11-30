55227790cb75f13cbcda764cc269efa02da652ae
Julien Viet <julien@julienviet.com>
2023-11-30 15:10:02 +0100

    Use @JsonGen annotation

--- src/main/generated/io/vertx/ext/stomp/StompClientOptionsConverter.java
+++ src/main/generated/io/vertx/ext/stomp/StompClientOptionsConverter.java
@@ -17,7 +17,7 @@ public class StompClientOptionsConverter {
   private static final Base64.Decoder BASE64_DECODER = JsonUtil.BASE64_DECODER;
   private static final Base64.Encoder BASE64_ENCODER = JsonUtil.BASE64_ENCODER;
 
-  public static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, StompClientOptions obj) {
+   static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, StompClientOptions obj) {
     for (java.util.Map.Entry<String, Object> member : json) {
       switch (member.getKey()) {
         case "host":
@@ -84,11 +84,11 @@ public class StompClientOptionsConverter {
     }
   }
 
-  public static void toJson(StompClientOptions obj, JsonObject json) {
+   static void toJson(StompClientOptions obj, JsonObject json) {
     toJson(obj, json.getMap());
   }
 
-  public static void toJson(StompClientOptions obj, java.util.Map<String, Object> json) {
+   static void toJson(StompClientOptions obj, java.util.Map<String, Object> json) {
     if (obj.getHost() != null) {
       json.put("host", obj.getHost());
     }
